c48f75f053427efa739c95e8cae118780e0c2483
Manan2307 <mananshah2307@gmail.com>
2022-05-09 18:23:09 +0530

    Project Complete

--- application.py
+++ application.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, request, redirect
-from flask_cors import CORS,cross_origin
+
 import pickle
 import pandas as pd
 import numpy as np
 
 
 app = Flask(__name__)
-cors=CORS(app)
 model=pickle.load(open('LinearRegressionModel.pkl','rb'))
 car = pd.read_csv("Cleaned_Car_DataSet.csv")
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    companies= sorted(car['company'].unique())
-    car_models= sorted(car['name'].unique())
-    year= sorted(car['year'].unique(),reverse=True)
-    fuel_type= car['fuel_type'].unique()
+    companies=sorted(car['company'].unique())
+    car_models=sorted(car['name'].unique())
+    year=sorted(car['year'].unique(),reverse=True)
+    fuel_type=car['fuel_type'].unique()
 
-
+    companies.insert(0,'Select Company')
     return render_template('index.html',companies=companies, car_models=car_models, years=year,fuel_types=fuel_type)
 
+
 @app.route('/predict',methods=['POST'])
-@cross_origin()
+
 def predict():
 
     company=request.form.get('company')
@@ -37,5 +37,7 @@ def predict():
 
     return str(np.round(prediction[0],2))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+if __name__=='__main__':
+    app.run()
